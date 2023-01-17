9264e52dbbd854622847c680df229fa10376a78d
Oleg Sobolev <heroh44444@gmail.com>
2023-01-17 12:14:33 +0300

    add benchmark

--- src/com/company/list/Benchmark.java
+++ src/com/company/list/Benchmark.java
@@ -1,16 +1,28 @@
 package com.company.list;
 
+import org.openjdk.jmh.annotations.*;
+
+@State(Scope.Benchmark)
 public class Benchmark {
+    @Param({"1000000", "10000000", "100000000"})
+    public int listSize;
 
+//    @Fork(value = 1, warmups = 1)
+//    @Warmup(iterations = 1)
 //    @org.openjdk.jmh.annotations.Benchmark
-    public int helloWorldBenchmark() {
-        return 1;
-    }
+//    @BenchmarkMode(Mode.AverageTime)
+//    public int helloWorldBenchmark() {
+//        return listSize;
+//    }
 
+
+    @Fork(value = 1, warmups = 1)
+    @Warmup(iterations = 1)
     @org.openjdk.jmh.annotations.Benchmark
+    @BenchmarkMode(Mode.AverageTime)
     public void init() {
         MyList list = new MyList();
-        for(int i = 1; i < 514; i++){
+        for(int i = 0; i < listSize; i++){
             list.add(i);
         }
     }
