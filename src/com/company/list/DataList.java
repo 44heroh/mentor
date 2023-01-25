b78e02dafb499346fea14ccfee74751095cf3891
Oleg Sobolev <heroh44444@gmail.com>
2023-01-25 22:43:36 +0300

    add new DataList + add junit in pom

---
+++ src/com/company/list/DataList.java
@@ -0,0 +1,165 @@
+package com.company.list;
+
+public class DataList<E> {
+    private static final int commonMulti = 2;
+    private Object[] elements;
+    public int size;
+    private static final int defaultSize = 10;
+
+    /**
+     * Конструктор
+     */
+    public DataList() {
+        this.elements = new Object[defaultSize];
+        this.size = 0;
+    }
+
+    /**
+     * Получаем массив элементов
+     * @return
+     */
+    public Object[] getElements() {
+        return elements;
+    }
+
+    /**
+     * Получаем размер
+     * @return
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * Добавление элемента в list
+     * @param e
+     */
+    public void add(E e) {
+        // если длина массива < размер + 1
+        if(elements.length < size + 1){
+            // модифицируем массив
+            modifyArr(new Object[(int) (size * commonMulti)], size);
+        }
+        // добавляем элемент в массив
+        elements[size++] = e;
+    }
+
+    /**
+     * Удаление элемента из list
+     * @param e
+     */
+    public void remove(E e) {
+        int sizeTmp = size;
+        // если половина длины массива >= размер - 1 и длина >= 10
+        if(elements.length / commonMulti >= size - 1 && elements.length >= defaultSize){
+            sizeTmp = (int) elements.length / commonMulti;
+        }
+        // проходим массив до установленного sizeTmp + 1
+        for(int i = 0; i < sizeTmp + 1; i++) {
+            if(elements[i] != null){
+                // если находим элемент
+                if(elements[i].equals(e)) {
+                    // то начиная с этого элемента приравниваем следующий элемент
+                    elements[i] = elements[i + 1];
+                }
+            }
+        }
+        // если установленный sizeTmp равен половине длины и длина >= 10
+        if((int) elements.length / commonMulti == sizeTmp && elements.length >= defaultSize){
+            // модифицируем массив
+            modifyArr(new Object[sizeTmp], sizeTmp);
+        }
+        size--;
+    }
+
+    /**
+     * Удаление по индексу
+     * @param index
+     */
+    public void removeByIndex(int index) {
+        int sizeTmp = size;
+        // если index != последнему индексу
+        if(index != size - 1) {
+            for(int i = 0; i < sizeTmp + 1; i++) {
+                if(i >= index) {
+                    elements[i] = elements[i + 1];
+                }
+            }
+        // если index == последнему индексу
+        } else if(index == size - 1) {
+            elements[index] = null;
+        }
+
+        // если установленный sizeTmp равен половине длины и длина >= 10
+        if((int) elements.length / commonMulti == size - 1 && elements.length >= defaultSize){
+            // модифицируем массив
+            modifyArr(new Object[(int) elements.length / commonMulti], (int) elements.length / commonMulti);
+        }
+        size--;
+    }
+
+    /**
+     * Получаем элемент
+     * @param index
+     * @return
+     */
+    public Object get(int index) {
+        return elements[index];
+    }
+
+    /**
+     * Получаем индекс элемента
+     * @param e
+     * @return
+     */
+    public Object getIndex(E e) {
+        for(int i = 0; i < elements.length; i++) {
+            if(elements[i].equals(e)) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * модифицирование массива
+     * @param newArr
+     * @param newSize
+     */
+    private void modifyArr(Object[] newArr, int newSize) {
+        elements = copy(newArr, newSize);
+    }
+
+    /**
+     * копирование массива
+     * @param newArr
+     * @param size
+     * @return
+     */
+    private Object[] copy(Object[] newArr, int size) {
+        for(int i = 0; i < size; i++) {
+            newArr[i] = elements[i];
+        }
+        return newArr;
+    }
+
+    /**
+     * Меняем местами элементы в массиве
+     * @param index1
+     * @param index2
+     */
+    public void swap(int index1, int index2) {
+        Object tmp = elements[index1];
+        elements[index1] = elements[index2];
+        elements[index2] = tmp;
+    }
+
+    /**
+     * Обновление элемента
+     * @param index
+     * @param value
+     */
+    public void updateElem(int index, Object value) {
+        elements[index] = value;
+    }
+}
