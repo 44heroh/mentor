83f24e7c49b46c7bba8a53b83b9ac3accf4dd14a
Oleg Sobolev <heroh44444@gmail.com>
2023-02-28 16:17:16 +0300

    create HeapSort

---
+++ src/com/company/sort/SortAlgorithm.java
@@ -0,0 +1,7 @@
+package com.company.sort;
+
+public abstract class SortAlgorithm {
+    public abstract void sort(int[] array);
+
+    public abstract void sortOptimized(int[] array);
+}
--- src/com/company/sort/heap/HeapSort.java
+++ src/com/company/sort/heap/HeapSort.java
@@ -1,352 +1,106 @@
 package com.company.sort.heap;
 
-public class HeapSort<E> {
-    private Object[] list;
-    private int size;
-    private int capacity;
-    private boolean isMax;
+import com.company.sort.SortAlgorithm;
 
-    public HeapSort(int capacity, boolean isMax) {
-        list = new Object[capacity];
-        size = 0;
-        this.capacity = capacity;
-        this.isMax = isMax;
+import java.util.Arrays;
+
+public class HeapSort extends SortAlgorithm {
+
+    @Override
+    public void sort(int[] array) {
+        sortRun(array, '<');
     }
 
-    /**
-     * Плучаем весь список
-     * @return
-     */
-    public Object[] getList() {
-        return list;
-    }
+    private void sortRun(int[] arr, char direction) {
+        int n = arr.length;
 
-    /**
-     * Получаем размер
-     * @return
-     */
-    public int getSize() {
-        return size;
-    }
-
-    /**
-     * Меняем местами элементы из списка
-     * @param index1
-     * @param index2
-     */
-    private void swap(int index1, int index2) {
-        Object tmp = list[index1];
-        list[index1] = list[index2];
-        list[index2] = tmp;
-    }
-
-    /**
-     * Получаем индекс родителя
-     * @param index
-     * @return
-     */
-    private int getParentIndex(int index) {
-        return (index - 1) / 2;
-    }
-
-    /**
-     * Получаем индекс левого элемента
-     * @param index
-     * @return
-     */
-    private int getLeftIndex(int index) {
-        return (2 * index) + 1;
-    }
-
-    /**
-     * Получаем индекс правого элемента
-     * @param index
-     * @return
-     */
-    private int getRightIndex(int index) {
-        return (2 * index) + 2;
-    }
-
-    /**
-     * Просеиваем элементы вверх
-     * @param index
-     */
-    private void siftUp(int index) {
-        // получаем индекс родителя
-        int parentIndex = getParentIndex(index);
-        // идем по списку пока индекс > 0 и текущий элемент < родительского
-        if(isMax) {
-            while (index > 0 && (int)list[parentIndex] < (int)list[index]) {
-                // меняем местами
-                swap(index, parentIndex);
-                // присваиваем индекс родителя -> index так как поменяли местами
-                index = parentIndex;
-                parentIndex = getParentIndex(index);
-            }
-        } else {
-            while (index > 0 && (int)list[parentIndex] > (int)list[index]) {
-                // меняем местами
-                swap(index, parentIndex);
-                // присваиваем индекс родителя -> index так как поменяли местами
-                index = parentIndex;
-                parentIndex = getParentIndex(index);
-            }
-        }
-    }
-
-    /**
-     *
-     * @param index
-     * @return
-     */
-    public boolean isLeaf(int index) {
-        if(index < (size / 2) || index <= size) {
-            return true;
+        // Заполняем кучу
+        for(int i = n/2; i >= 0; i--) {
+            heapify(arr, n, i, direction);
+            displayHeap(arr);
+            System.out.println(Arrays.toString(arr));
         }
 
-        return false;
+        System.out.println();
+        // Получаем один за одним элемент из кучи
+        for(int i = n - 1; i >= 0; i--) {
+            // Перемещаем корень в конец
+            swap(arr, i, 0);
+            // Вызываем на уменьшенной куче c 0 до i
+            heapify(arr, i, 0, direction);
+            displayHeap(arr);
+            System.out.println(Arrays.toString(arr));
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(arr));
+        displayHeap(arr);
     }
 
-    /**
-     * Просеиваем вниз
-     * @param index
-     */
-    private void siftDown(int index) {
-        int leftIndex = getLeftIndex(index);
-        int rightIndex = getRightIndex(index);
-        // присваиваем наибольшему текущий индекс
-        int large = index;
+    private void heapify(int[] arr, int size, int index, char direction) {
+        // индекс левого элемента
+        int left = (index * 2) + 1;
+        // индекс правого элемента
+        int right = (index * 2) + 2;
+        // устанавливаем основной элемент
+        int main = index;
 
-        // если левый < размера массива и != null
-        if(leftIndex < list.length && list[leftIndex] != null) {
-            // если максимальная куча
-            if(isMax){
-                // если левый > наибольшего
-                if((int)list[leftIndex] > (int)list[large])
-                    large = leftIndex;
-            // если минимальная куча
-            } else {
-                // если левый < наибольшего
-                if((int)list[leftIndex] < (int)list[large])
-                    large = leftIndex;
-            }
-
-        }
-        // если правый < размера массива и != null
-        if(rightIndex < list.length && list[rightIndex] != null) {
-            // если максимальная куча
-            if(isMax) {
-                // если правый > наибольшего
-                if ((int) list[rightIndex] > (int) list[large])
-                    large = rightIndex;
-            // если минимальная куча
-            } else {
-                // если правый < наибольшего
-                if((int)list[rightIndex] < (int)list[large])
-                    large = rightIndex;
+        // если левый индекс < размера
+        if(left < size){
+            if(direction == '>') {
+                // основной элемент > левого элемента
+                if(arr[main] > arr[left])
+                    main = left;
+            } else if(direction == '<') {
+                // основной элемент < левого элемента
+                if(arr[main] < arr[left])
+                    main = left;
             }
         }
 
-        // наибольший != текущкму
-        if(index != large) {
-            // меняем местами
-            swap(index, large);
-            // продолжаем просеивание вниз
-            siftDown(large);
-        }
-    }
-
-    /**
-     * Добавление элемента
-     * @param e
-     */
-    public void add1(E e) {
-        if(capacity <= size) {
-            // изменяем массив на новый массив с размером(size + 1) и длиной size
-            modifyArr(new Object[size + 1], size);
-        }
-        // добавляем в список
-        list[size] = e;
-        // просеиваем вверх
-        siftUp(size);
-
-        if(capacity == size) {
-            // удаляем минимальный элемент
-            removeMinElement();
-            // изменяем массив на новый массив с размером(size + 1) и длиной size + 1
-            modifyArr(new Object[size + 1], size + 1);
-            // просеиваем вверх
-            siftUp(size);
-        }
-        // увеличиваем размер
-        size++;
-    }
-
-    /**
-     * Добавление элемента
-     * @param e
-     */
-    public void add(E e) {
-
-        if(capacity == size) {
-            if((int)list[size] < (int)e) {
-                size--;
+        // если правый индекс < размера
+        if(right < size) {
+            if(direction == '>') {
+                // основной элемент > правого элемента
+                if(arr[main] > arr[right])
+                    main = right;
+            }else if(direction == '<') {
+                // основной элемент < правого элемента
+                if(arr[main] < arr[right])
+                    main = right;
             }
         }
 
-        // если максимальная куча
-        if(isMax) {
-            // добавляем в список
-            list[size] = e;
-            // просеиваем вверх
-            siftUp(size);
-        // если минимальная куча
-        } else {
-            if(size > 0) {
-                // запоминаем значение size
-                int count = size;
-                // сдвигаем все элементы вправо пока count > 0
-                while(count > 0) {
-                    list[count] = list[count - 1];
-                    // уменьшаем count на 1
-                    count--;
-                }
-            }
-            // добавляем в список
-            list[0] = e;
-            // просеиваем вниз
-            siftDown(0);
+        // index не равен main
+        if(index != main) {
+            // меняем местами текущий и основной
+            swap(arr, index, main);
+            // вызываем на затронутом поддереве
+            heapify(arr, size, main, direction);
         }
-
-        // увеличиваем размер
-        size++;
     }
 
-    /**
-     * модифицирование массива
-     * @param newArr
-     * @param newSize
-     */
-    private void modifyArr(Object[] newArr, int newSize) {
-        list = copy(newArr, newSize);
+    private static void swap(int[] arr, int x, int y) {
+        int tmp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = tmp;
     }
 
-
-    /**
-     * копирование массива
-     * @param newArr
-     * @param size
-     * @return
-     */
-    private Object[] copy(Object[] newArr, int size) {
-        for(int i = 0; i < size; i++) {
-            newArr[i] = list[i];
-        }
-        return newArr;
-    }
-
-    /**
-     * Забираем верхний элемент
-     * @return
-     */
-    public int top() {
-        Integer top = (int)list[0];
-        remove((E) top);
-        return top;
-    }
-
-    /**
-     * Возвращаем наибольший элемент
-     * @return
-     */
-    public int peek() {
-        return (int)list[0];
-    }
-
-    /**
-     * Поиск индекса минимального значения в массиве
-     * @return
-     */
-    private int findMinElementIndex() {
-        int minimum = Integer.MAX_VALUE;
-        Integer minIndex = null;
-        for(int i = list.length - 1; i >= 0; i--) {
-            // ищем минимальный элемент
-            if((int)list[i] <= minimum){
-                minimum = (int)list[i];
-                minIndex = i;
-            }
-        }
-
-        return minIndex;
-    }
-
-    /**
-     * Удаление по индексу
-     */
-    public void removeMinElement() {
-        // Находим индекс минимального элемента
-        int minIndex = findMinElementIndex();
-
-        int sizeTmp = size;
-        // Проходим по массиву
-        for(int i = minIndex; i < list.length - 1; i++) {
-            // то каждый элемент начиная с него приравниваем следующему
-            if(i >= minIndex) {
-                list[i] = list[i + 1];
-            }
-        }
-        list[size] = null;
-        // уменьшшаем размер
-        size--;
-    }
-
-    /**
-     * Удаление элемента
-     * @param e
-     */
-    public void remove(E e) {
-        Integer searchIndex = null;
-        // ищим элемент
-        for(int i = 0; i < list.length; i++) {
-            if(list[i].equals(e)) {
-                searchIndex = i;
-                // обновляем искомый элемент на нижний для просеивания вниз
-                list[searchIndex] = list[size - 1];
-                // удаляем последний элемент
-                list[size - 1] = null;
-                // уменьшаем размер
-                size--;
-                break;
-            }
-        }
-
-        if (searchIndex == 0 || (int)list[searchIndex] < (int)list[getParentIndex(searchIndex)]) {
-            // просеиваем вниз
-            siftDown(searchIndex);
-        } else {
-            // просеиваем вверх
-            siftUp(searchIndex);
-        }
-
-    }
-
-    public void displayHeap(){
+    public static void displayHeap(int[] arr){
         int nBlanks = 32;
         int itemsPerRow = 1;
         int column = 0;
         int j = 0; // Текущий элемент
         String dots = "...............................";
         System.out.println(dots+dots); // Верхний пунктир
-
+        int size = arr.length;
         while(size > 0){ // Для каждого элемента пирамиды
             if(column == 0){ // Первый элемент в строке?
                 for(int k = 0; k < nBlanks; k++) // Предшествующие пробелы
                     System.out.print(' ');
             }
             // Вывод элемента
-            if(list[j] != null)
-                System.out.print(list[j]);
+            System.out.print(arr[j]);
+
 
             if(++j == size)  // Вывод завершен?
                 break;
@@ -362,5 +116,9 @@ public class HeapSort<E> {
             }
         }
         System.out.println("\n" + dots + dots);
+    }
+
+    public void sortOptimized(int[] array) {
+
     }
 }
